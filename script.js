892390f07de6bbe68565a55c6dbf08093adbed88
Martin Selin <seleeto@gmail.com>
2026-03-18 11:02:56 +0100

    Initial commit - benchpress calculator

---
+++ script.js
@@ -0,0 +1,42 @@
+const form = document.getElementById("rm-form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const weight = parseFloat(document.getElementById("weight").value);
+  const reps = parseInt(document.getElementById("reps").value);
+  const tableBody = document.getElementById("percentage-body");
+  tableBody.innerHTML = "";
+
+  const oneRM = weight * (1 + reps / 30);
+  const oneRepMaxOutput = document.getElementById("one-rep-max");
+
+  oneRepMaxOutput.textContent = oneRM.toFixed(1);
+
+  console.log(oneRM);
+
+  const percentages = [100, 95, 90, 85, 80, 75, 70, 65, 60];
+
+  percentages.forEach(function (percent) {
+    const tr = document.createElement("tr");
+    const td1 = document.createElement("td");
+    const td2 = document.createElement("td");
+
+    const weightAtPercent = oneRM * (percent / 100);
+
+    td1.textContent = percent + "%";
+    td2.textContent = weightAtPercent.toFixed(1);
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tableBody.appendChild(tr);
+  });
+
+const repTableBody = document.getElementById("repmax-body");
+
+
+
+
+
+
+});
